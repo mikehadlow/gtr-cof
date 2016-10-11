720fa2dc6ab39029352f10fddec87145829d51fe
Mike Hadlow <mike@suteki.co.uk>
2016-10-11 15:30:22 +0100

    Reference D3, draw a circle.

--- gtr-cof.js
+++ gtr-cof.js
@@ -1,11 +1,15 @@
-var Startup = (function () {
-    function Startup() {
+///<reference path="node_modules/@types/d3/index.d.ts" />
+var gtrcof;
+(function (gtrcof) {
+    function init() {
+        var cof = d3.select("#cof").append("g");
+        cof.append("circle")
+            .attr("r", "300")
+            .attr("cx", "300")
+            .attr("cy", "300");
+        console.log("init done!");
     }
-    Startup.main = function () {
-        console.log("Hello World!");
-        return 0;
-    };
-    return Startup;
-}());
-Startup.main();
+    gtrcof.init = init;
+})(gtrcof || (gtrcof = {}));
+gtrcof.init();
 //# sourceMappingURL=gtr-cof.js.map
--- gtr-cof.ts
+++ gtr-cof.ts
@@ -1,8 +1,16 @@
-class Startup {
-    public static main() : number {
-        console.log("Hello World!");
-        return 0;
+///<reference path="node_modules/@types/d3/index.d.ts" />
+
+namespace gtrcof {
+    
+    export function init() {
+        let cof = d3.select("#cof").append("g");
+        cof.append("circle")
+            .attr("r", "300")
+            .attr("cx", "300")
+            .attr("cy", "300");
+        console.log("init done!");
     }
+
 }
 
-Startup.main();
+gtrcof.init();
