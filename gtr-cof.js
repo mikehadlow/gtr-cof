608fd4ac82d90638b4db20cd50db435536283229
Mike Hadlow <mike@suteki.co.uk>
2016-10-12 16:20:02 +0100

    Add spokes to circle.

--- gtr-cof.js
+++ gtr-cof.js
@@ -4,17 +4,46 @@ var gtrcof;
     function init() {
         var pad = 10;
         var radius = 300;
-        var cof = d3.select("#cof").append("g");
+        var innerRadius = radius - 150;
+        var cof = d3
+            .select("#cof")
+            .append("g")
+            .attr("transform", "translate(" + (radius + pad) + ", " + (radius + pad) + ")");
         cof.append("circle")
             .attr("r", radius)
-            .attr("cx", radius + pad)
-            .attr("cy", radius + pad)
             .attr("fill", "none")
+            .attr("stroke", "black")
+            .attr("stroke-width", "2");
+        cof.append("circle")
+            .attr("r", innerRadius)
+            .attr("fill", "none")
+            .attr("stroke", "black")
+            .attr("stroke-width", "2");
+        cof.selectAll("path")
+            .data(angles(12))
+            .enter()
+            .append("path")
+            .attr("d", function (x) { return radialGenerator(innerRadius, radius, x); })
             .attr("stroke", "black")
             .attr("stroke-width", "2");
         console.log("init done!");
     }
     gtrcof.init = init;
+    function polarToCart(r, radians) {
+        return [r * Math.cos(radians), r * Math.sin(radians)];
+    }
+    function radialGenerator(inner, outter, radians) {
+        var innerCart = polarToCart(inner, radians);
+        var outterCart = polarToCart(outter, radians);
+        return "M " + innerCart[0] + " " + innerCart[1] + " L " + outterCart[0] + " " + outterCart[1];
+    }
+    function angles(count) {
+        var items = [];
+        for (var i = 0; i < count; i++) {
+            items.push(i * (Math.PI * (2 / count)));
+        }
+        return items;
+    }
 })(gtrcof || (gtrcof = {}));
 gtrcof.init();
 //# sourceMappingURL=gtr-cof.js.map
--- gtr-cof.ts
+++ gtr-cof.ts
@@ -5,17 +5,53 @@ namespace gtrcof {
     export function init() {
         let pad = 10;
         let radius = 300;
-        let cof = d3.select("#cof").append("g");
+        let innerRadius = radius - 150;
+
+        let cof = d3
+            .select("#cof")
+            .append("g")
+            .attr("transform", "translate(" + (radius + pad) +", " + (radius + pad) + ")");
+
         cof.append("circle")
             .attr("r", radius)
-            .attr("cx", radius + pad)
-            .attr("cy", radius + pad)
             .attr("fill", "none")
             .attr("stroke", "black")
             .attr("stroke-width", "2");
+
+        cof.append("circle")
+            .attr("r", innerRadius)
+            .attr("fill", "none")
+            .attr("stroke", "black")
+            .attr("stroke-width", "2");
+
+        cof.selectAll("path")
+            .data(angles(12))
+            .enter()
+            .append("path")
+            .attr("d", function(x) { return radialGenerator(innerRadius, radius, x); })
+            .attr("stroke", "black")
+            .attr("stroke-width", "2");
+            
         console.log("init done!");
     }
 
+    function polarToCart(r: number, radians: number) : [number, number] {
+        return [r * Math.cos(radians), r * Math.sin(radians)];
+    }
+    
+    function radialGenerator(inner: number, outter: number, radians: number) : string {
+        let innerCart = polarToCart(inner, radians);
+        let outterCart = polarToCart(outter, radians);
+        return "M " + innerCart[0] + " " + innerCart[1] + " L " + outterCart[0] + " " + outterCart[1];
+    }
+    
+    function angles(count: number) : number[] {
+        let items : Array<number> = [];
+        for(let i:number = 0; i < count; i++) {
+            items.push(i * (Math.PI * (2 / count)));
+        }
+        return items;
+    }
 }
 
 gtrcof.init();
