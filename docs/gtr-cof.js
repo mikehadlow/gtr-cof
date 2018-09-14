da57c266734a5c8e984afbc226c94b7b914e6ca9
Mike Hadlow <mike@suteki.co.uk>
2018-09-14 15:59:59 +0100

    Revert change to MIDI module.

--- docs/gtr-cof.js
+++ docs/gtr-cof.js
@@ -1235,11 +1235,10 @@ var midiControl;
             console.log("Browser does not support MIDI.");
             return;
         }
-        window.navigator.requestMIDIAccess()
+        nav.requestMIDIAccess()
             .then((midiAccess) => {
             console.log("MIDI Ready!");
             for (let entry of midiAccess.inputs) {
-                console.log("MIDI input device: " + entry[1].id);
                 entry[1].onmidimessage = onMidiMessage;
             }
         })
@@ -1248,29 +1247,24 @@ var midiControl;
         });
     }
     midiControl.init = init;
-    let noteNames = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
     function onMidiMessage(midiEvent) {
         let data = midiEvent.data;
         if (data.length === 3) {
-            // status is the first byte.
             let status = data[0];
             // command is the four most significant bits of the status byte.
             let command = status >>> 4;
-            // channel 0-15 is the lower four bits.
-            let channel = status & 0xF;
-            console.log(`$Command: ${command.toString(16)}, Channel: ${channel.toString(16)}`);
-            // just look at note on and note off messages.
-            if (command === 0x9 || command === 0x8) {
-                // note number is the second byte.
-                let note = data[1];
-                // velocity is the thrid byte.
-                let velocity = data[2];
-                let commandName = command === 0x9 ? "Note On " : "Note Off";
-                // calculate octave and note name.
-                let octave = Math.trunc(note / 12);
-                let noteName = noteNames[note % 12];
-                console.log(`${commandName} ${noteName}${octave} ${velocity}`);
+            //let octave = Math.trunc(data[1] / 12);
+            // MIDI starts with C0 = 0, but guitar dashboard index 0 = A, so add three to the midi note number.
+            let index = (data[1] + 3) % 12;
+            if (command === 0x9) {
+                // MIDI note on.
+                currentToggledIndexes = currentToggledIndexes | Math.pow(2, index);
             }
+            if (command === 0x8) {
+                // MIDI note off.
+                currentToggledIndexes = currentToggledIndexes & ~(Math.pow(2, index));
+            }
+            events.midiNote.publish({ toggledIndexes: currentToggledIndexes });
         }
     }
 })(midiControl || (midiControl = {}));
--- src/midi-module.ts
+++ src/midi-module.ts
@@ -13,11 +13,10 @@ namespace midiControl {
             return;
         }
 
-        window.navigator.requestMIDIAccess()
+        nav.requestMIDIAccess()
             .then((midiAccess) => {
                 console.log("MIDI Ready!");
                 for(let entry of midiAccess.inputs) {
-                    console.log("MIDI input device: " + entry[1].id)
                     entry[1].onmidimessage = onMidiMessage;
                 }
             })
@@ -26,35 +25,24 @@ namespace midiControl {
             });
     }
 
-let noteNames: string[] = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
-
-function onMidiMessage(midiEvent: WebMidi.MIDIMessageEvent): void {
-    let data: Uint8Array = midiEvent.data;
-    if(data.length === 3) {
-        // status is the first byte.
-        let status = data[0];
-        // command is the four most significant bits of the status byte.
-        let command = status >>> 4;
-        // channel 0-15 is the lower four bits.
-        let channel = status & 0xF;
-
-        console.log(`$Command: ${command.toString(16)}, Channel: ${channel.toString(16)}`);
-
-        // just look at note on and note off messages.
-        if(command === 0x9 || command === 0x8) {
-            // note number is the second byte.
-            let note = data[1];
-            // velocity is the thrid byte.
-            let velocity = data[2];
-
-            let commandName = command === 0x9 ? "Note On " : "Note Off";
-
-            // calculate octave and note name.
-            let octave = Math.trunc(note / 12);
-            let noteName = noteNames[note % 12];
-
-            console.log(`${commandName} ${noteName}${octave} ${velocity}`);
+    function onMidiMessage(midiEvent: WebMidi.MIDIMessageEvent): void {
+        let data = midiEvent.data;
+        if(data.length === 3) {
+            let status = data[0];
+            // command is the four most significant bits of the status byte.
+            let command = status >>> 4;
+            //let octave = Math.trunc(data[1] / 12);
+            // MIDI starts with C0 = 0, but guitar dashboard index 0 = A, so add three to the midi note number.
+            let index = (data[1] + 3) % 12;
+            if(command === 0x9) {
+                // MIDI note on.
+                currentToggledIndexes = currentToggledIndexes | 2**index;
+            }
+            if(command === 0x8) {
+                // MIDI note off.
+                currentToggledIndexes = currentToggledIndexes & ~(2**index);
+            }
+            events.midiNote.publish({ toggledIndexes: currentToggledIndexes });
         }
     }
-}
 }
