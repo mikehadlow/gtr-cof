3a30ea72759418860013d25f564e8e4e8bfc737e
Mike Hadlow <mike@suteki.co.uk>
2019-04-08 21:45:21 +0100

    Build for new 7 string tuning.

--- docs/gtr-cof.js
+++ docs/gtr-cof.js
@@ -1149,6 +1149,7 @@ var tuning;
         { tuning: "DADGBE", dots: guitarDots, description: "Guitar Drop D" },
         { tuning: "DADGAD", dots: guitarDots, description: "Celtic Tuning" },
         { tuning: "CGDAEA", dots: guitarDots, description: "Guitar Fripp NST" },
+        { tuning: "BEADGBE", dots: guitarDots, description: "Guitar 7 string" },
         { tuning: "EADG", dots: guitarDots, description: "Bass Standard" },
         { tuning: "DADG", dots: guitarDots, description: "Bass Drop D" },
         { tuning: "DGBD", dots: guitarDots, description: "Banjo" },
