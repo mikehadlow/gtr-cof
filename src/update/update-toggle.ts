4b3b19338e62b22e56c6033338d743cf73d299b7
Mike Hadlow <mike@suteki.co.uk>
2026-03-10 10:47:29 +0000

    Update toggle implementation

--- src/update/update-toggle.ts
+++ src/update/update-toggle.ts
@@ -1,6 +1,9 @@
 import { type Update as UpdateModel } from "../types";
 import { type Model } from "../model";
+import { updateScale } from "."
 
 export const Update: UpdateModel<Model, { id: "Toggle", index: number }> = (model, msg) => {
-    return model;
+    const current = model.state;
+    current.toggledIndexes = current.toggledIndexes ^ 2 ** msg.index;
+    return updateScale(current);
 }
