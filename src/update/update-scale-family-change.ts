d892d9afce743fbd034eae7ad10c7ce5375b1657
Mike Hadlow <mike@suteki.co.uk>
2026-03-10 10:51:30 +0000

    Update scale family changed implementation

--- src/update/update-scale-family-change.ts
+++ src/update/update-scale-family-change.ts
@@ -1,7 +1,12 @@
 import { type Update as UpdateModel } from "../types";
 import { type Model } from "../model";
 import * as music from '../music';
+import { updateScale } from ".";
 
 export const Update: UpdateModel<Model, { id: "ScaleFamilyChange", scaleFamily: music.ScaleFamily }> = (model, msg) => {
-    return model;
+    const current = model.state;
+    current.scaleFamilyIndex = msg.scaleFamily.index;
+    current.modeIndex = msg.scaleFamily.defaultModeIndex;
+    current.chordIndex = -1
+    return updateScale(current);
 }
