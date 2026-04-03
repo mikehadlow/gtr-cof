f24e1ac435cb2c48cf0e9b584017270b35a4e8e8
Mike Hadlow <mike@suteki.co.uk>
2026-04-03 13:10:42 +0100

    Fix bug when toggle is cleared on chord interval change

--- src/update/update-chord-interval-change.test.ts
+++ src/update/update-chord-interval-change.test.ts
@@ -10,9 +10,18 @@ describe("update-chord-interval-change", () => {
         expect(result.state.chordIntervals).toEqual([0, 2, 4, 6]);
     });
 
-    test("clears toggledIndexes", () => {
-        const model = updateScale({ ...defaultState, toggledNotesBitmask: (2 ** 0) | (2 ** 4) });
+    test("does not clear toggledIndexes when no chord is selected", () => {
+        const toggledNotesBitmask = (2 ** 0) | (2 ** 4);
+        const model = updateScale({ ...defaultState, toggledNotesBitmask });
         const result = Update(model, { id: "ChordIntervalChange", chordIntervals: [0, 2, 4] });
-        expect(result.state.toggledNotesBitmask).toBe(0);
+        expect(result.state.toggledNotesBitmask).toBe(toggledNotesBitmask);
+    });
+
+    test("clears toggledIndexes when chord is selected", () => {
+        const toggledNotesBitmask = (2 ** 3) | (2 ** 4);
+        const cSevenChord = (2 ** 0) | (2 ** 4) | (2 ** 7) | (2 ** 11); // C7 toggles
+        const model = updateScale({ ...defaultState, toggledNotesBitmask, chordIndex: 0 });
+        const result = Update(model, { id: "ChordIntervalChange", chordIntervals: [0, 2, 4, 6] });
+        expect(result.state.toggledNotesBitmask).toBe(cSevenChord);
     });
 });
--- src/update/update-chord-interval-change.ts
+++ src/update/update-chord-interval-change.ts
@@ -5,6 +5,8 @@ import { updateScale } from "./updateScale";
 export const Update: UpdateModel<Model, { id: "ChordIntervalChange"; chordIntervals: number[] }> = (model, msg) => {
     const current = model.state;
     current.chordIntervals = msg.chordIntervals;
-    current.toggledNotesBitmask = 0;
+    if (current.chordIndex !== -1) {
+        current.toggledNotesBitmask = 0;
+    }
     return updateScale(current);
 };
