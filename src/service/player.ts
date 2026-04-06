5d02a8a6228956e592db4d58173dd91e42b8efe0
Mike Hadlow <mike@suteki.co.uk>
2026-04-06 13:10:50 +0100

    play notes relative to the tonic, so scales always ascend

--- src/service/player.ts
+++ src/service/player.ts
@@ -6,18 +6,23 @@ import type { Service } from "../types";
 type Toggle = Extract<Msg, { id: "Toggle" }>;
 
 const defaultOctave = 57; // A3 MIDI note
+const octave = 12;
 
 export const service: Service<Model, Toggle, Msg> = (model: Model, msg: Toggle, raise: (msg: Msg) => void): void => {
-    console.log(`Toggle message: ${msg.id}, index: ${msg.index}`);
     const node = getNodeAtIndex(model.music.nodes, msg.index);
     if (node.toggle) {
         // toggling on note, so it should be played
         console.log(`ON  node ${node.scaleNote.note.label}`);
         raise({
             id: "Play",
-            midiNote: msg.index + defaultOctave,
+            midiNote: getMidiNote(model, msg),
         });
-    } else {
-        console.log(`OFF node ${node.scaleNote.note.label}`);
     }
 };
+
+// Play a note relative to the current tonic
+function getMidiNote({ state }: Model, msg: Toggle): number {
+    const midiNote = msg.index < state.index ? defaultOctave + octave + msg.index : defaultOctave + msg.index;
+    console.log(`getMidiNote: state.index: ${state.index}, msg.index: ${msg.index}`);
+    return midiNote;
+}
